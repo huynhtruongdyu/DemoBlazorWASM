16f4a77a134dd3c176981c2241f6f7fe9c2cb795
Duy Huynh <huynhtruongdyu@gmail.com>
2023-06-07 13:13:39 +0700

    Initial commit

---
+++ Shared/Components/ButtonComponent.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazorWASM.Shared.Components
+{
+    public partial class ButtonComponent
+    {
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Classes { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback OnClickEvent { get; set; }
+
+        public async Task HandleOnClick() => await OnClickEvent.InvokeAsync();
+    }
+}
